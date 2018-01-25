533622870eb9695a2660a1830033d63ad4d6218c
Jakob Ristner <jakob.ristner@itggot.se>
2018-01-25 13:07:12 +0100

    Minor stuff, linking css etc.

--- website-file-structure-creator.rb
+++ website-file-structure-creator.rb
@@ -2,7 +2,6 @@ require 'fileutils'
 puts "Enter the name of the file directory"
 name = gets.chomp.capitalize
 
-
 FileUtils::mkdir_p "#{name}"
 FileUtils::mkdir_p "#{name}/misc"
 FileUtils::mkdir_p "#{name}/Original_files"
@@ -11,6 +10,6 @@ FileUtils::mkdir_p "#{name}/site"
 FileUtils::mkdir_p "#{name}/site/css"
 FileUtils::mkdir_p "#{name}/site/img"
 FileUtils::mkdir_p "#{name}/site/js"
+
 File.new("#{name}/Site/index.html", "w+")
 File.new("#{name}/site/css/style.css", "w+")
-
